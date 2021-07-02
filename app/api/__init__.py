482937e0acdf0ef8fe02f97c08c4639ac8a17e95
zhongyehai <zhongyehai>
2021-07-02 15:14:15 +0800

    添加文件管理功能

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -6,7 +6,7 @@ from flask_login import current_user
 
 api = Blueprint('api', __name__)
 
-from . import (file, errors)
+from . import (errors)
 from app.api.user import views
 from app.api.project import views
 from app.api.module import views
@@ -18,6 +18,7 @@ from app.api.apiMsg import views
 from app.api.case import views
 from app.api.step import views
 from app.api.tools import views
+from app.api.tools import fileView
 from app.api.home import view
 from app.api.config import views
 
@@ -32,7 +33,7 @@ def before_request():
 @api.after_request
 def after_request(response_obj):
     """ 后置钩子函数，每个请求最后都会经过此函数 """
-    if 'downloadFile' in request.path:
+    if 'download' in request.path:
         return response_obj
     result = copy.copy(response_obj.response)
     if isinstance(result[0], bytes):
